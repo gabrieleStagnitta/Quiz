70e9f81ef93772a85da644ba8aff09a6f31cbc7e
Gabriele Stagnitta <102358284+gabrieleStagnitta@users.noreply.github.com>
2026-08-30 10:53:16 +0200

    Modify static file serving path in server.js
    Updated static file serving to use path.join for reliability.

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const http = require('http');
 const { Server } = require('socket.io');
 const setupSockets = require('./src/sockets');
 
-// Carica l'interfaccia grafica
-app.use(express.static('public'));
+// 1. Modifica applicata qui: path.join assicura che trovi sempre la cartella
+app.use(express.static(path.join(__dirname, 'public')));
 
 const server = http.createServer(app);
 
